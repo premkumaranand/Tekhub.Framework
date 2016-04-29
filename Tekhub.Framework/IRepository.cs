51a73301fb6a79a1e8e4fdcd5f5359345a10fd10
premkumaranand <aa.premkumar@gmail.com>
2016-04-29 09:48:12 +0100

    Init

---
+++ Tekhub.Framework/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tekhub.Framework
+{
+    public interface IRepository<TId, TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns>ID of the new object</returns>
+        TId Add(TEntity dto);
+        IReadOnlyList<TId> AddRange(IEnumerable<TEntity> dtos);
+        bool Exists(TId id);
+        TEntity Get(TId id);
+        IReadOnlyList<TEntity> GetRange(IEnumerable<TId> ids);
+        void Update(TEntity dto);
+        void Delete(TEntity dto);
+    }
+}
